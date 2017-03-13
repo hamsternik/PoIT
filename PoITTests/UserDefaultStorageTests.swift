74caab4876365c022148f0ce76bff584515bbb96
Nikita Khomitsevich <hamsternik9@gmail.com>
2017-03-14 01:35:07 +0200

    Added XCTestFramework with supposed test case file for persistence layer

---
+++ PoITTests/UserDefaultStorageTests.swift
@@ -0,0 +1,26 @@
+//
+//  UserDefaultStorageTests.swift
+//  PoIT
+//
+//  Created by Nikita Khomitsevich on 3/14/17.
+//  Copyright © 2017 Nikita Khomitsevich. All rights reserved.
+//
+
+import XCTest
+
+class UserDefaultStorageTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // TODO: add `UserDefaultsStorage` initialization
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        // TODO: !Impl
+    }
+
+}
